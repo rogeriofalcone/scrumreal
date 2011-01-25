12b48bac5a03119012a9dc7fa9f8084c571a2a20
Fábio Cerqueira <steelday@gmail.com>
2011-01-25 13:32:02 -0300

    view para gerar pdf baseado no post do postit

--- scrumreal/views.py
+++ scrumreal/views.py
@@ -1,11 +1,23 @@
-from flask import render_template, url_for, Response
+from flask import render_template, url_for, Response, request
+
 from pyfpdf import FPDF
 
 from app import app
+from scrumreal.utils import PostIt
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/postit', methods=['POST'])
+def postit():
+    postits = PostIt.load_post(request.form)
+    pdf_postits = PostIt.make_pdf(postits)
+    response = Response(response=pdf_postits, mimetype="application/pdf")
+    response.headers['Content-Disposition'] = 'filename=postits.pdf'
+    return response
+
 
 @app.route('/about')
 def about():
