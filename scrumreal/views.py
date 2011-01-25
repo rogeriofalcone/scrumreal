e26fa29262f14030e3a2ce39afee36198076d483
Fábio Cerqueira <steelday@gmail.com>
2011-01-25 01:51:53 -0300

    adicionado exemplo de uso do pyfpdf + gae + flask

--- scrumreal/views.py
+++ scrumreal/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, Response
+from pyfpdf import FPDF
 
 from app import app
 
@@ -9,3 +10,13 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/test')
+def test():
+    pdf=FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial','B',16)
+    pdf.cell(40,10,'Hello World!')
+    response = Response(response=pdf.output(dest='S'), mimetype="application/pdf")
+    response.headers['Content-Disposition'] = 'filename=postits.pdf'
+    return response
