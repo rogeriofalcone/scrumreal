b8952aca3f354c8e466cd8d3d8643bbbb514191b
Fábio Cerqueira <steelday@gmail.com>
2011-01-26 12:56:42 -0300

    Removido view de teste

--- scrumreal/views.py
+++ scrumreal/views.py
@@ -23,12 +23,3 @@ def postit():
 def about():
     return render_template('about.html')
 
-@app.route('/test')
-def test():
-    pdf=FPDF()
-    pdf.add_page()
-    pdf.set_font('Arial','B',16)
-    pdf.cell(40,10,'Hello World!')
-    response = Response(response=pdf.output(dest='S'), mimetype="application/pdf")
-    response.headers['Content-Disposition'] = 'filename=postits.pdf'
-    return response
